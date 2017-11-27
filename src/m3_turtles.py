677f0c2d59d33cb5c2363d92989659c974848e04
tianxihe <het@rose-hulman.edu>
2017-11-27 15:06:49 -0500

    added my name 2017/11/27

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -14,7 +14,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 """
 ########################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #  (Yes, that means for YOU to DO things per these instructions:)
 #
 # On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -63,21 +63,28 @@ dave = rg.SimpleTurtle()
 dave.forward(100)
 dave.left(90)
 dave.forward(200)
-
+dave.right(100)
+dave. forward(100)
 # ----------------------------------------------------------------------
 # Construct a new turtle and ask it to do things.
 # ----------------------------------------------------------------------
 matt = rg.SimpleTurtle('turtle')
 matt.pen = rg.Pen('red', 30)
 matt.speed = 10  # Faster
-matt.backward(50)
+matt.forward(50)
 matt.left(90)
+matt.forward(50)
+matt.left(90)
+matt.forward(50)
+matt.right(90)
+matt.forward(50)
+matt.right(90)
 matt.forward(50)
 
 
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -91,7 +98,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -109,10 +116,22 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
+ZACK=rg.SimpleTurtle('turtle')
+ZACK.pen=rg.Pen("Blue",20)
+ZACK.speed= 20
+ZACK.forward(80)
+ZACK.left(90)
+ZACK.forward(100)
+ZACK.backward(50)
+ZACK.right(90)
+ZACK.forward(50)
+ZACK.right(90)
+ZACK.forward(50)
+
 
 ########################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
